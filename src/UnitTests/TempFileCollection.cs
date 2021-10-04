7f4b2c5e9ca176360919ae6fcf779f6f8ece151d
Chris Lovett <clovett@microsoft.com>
2021-10-04 13:10:51 -0700

    add fragment test

---
+++ src/UnitTests/TempFileCollection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace UnitTests
+{
+    class TempFileCollection : IDisposable
+    {
+        List<string> tempFiles = new List<string>();
+
+        public string CreateTempFile(string contents)
+        {
+            var name = System.IO.Path.GetTempFileName();
+            File.WriteAllText(name, contents);
+            tempFiles.Add(name);
+            return name;
+        }
+
+        public void Dispose()
+        {
+            foreach(var file in tempFiles)
+            {
+                try
+                {
+                    if (System.IO.File.Exists(file))
+                    {
+                        File.Delete(file);
+                    }
+                } catch { }
+            }
+        }
+    }
+}
--- src/UnitTests/UnitTest1.cs
+++ src/UnitTests/UnitTest1.cs
@@ -10,7 +10,7 @@ namespace UnitTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void SimpleAddition()
         {
             XmlDocument doc1 = new XmlDocument();
             doc1.LoadXml("<a></a>");
@@ -29,9 +29,43 @@ namespace UnitTests
             XmlDocument result = new XmlDocument();
             result.LoadXml(sw.ToString());
 
-            var inner = result.DocumentElement.InnerXml; // avoid comparing the hash.
-            var expected = "<xd:node match=\"1\" xmlns:xd=\"http://schemas.microsoft.com/xmltools/2002/xmldiff\"><xd:add><b /></xd:add></xd:node>";
+            var inner = ToComparibleString(result);
+            var expected = "<xd:xmldiff version=\"1.0\" options=\"None\" fragments=\"no\" xmlns:xd=\"http://schemas.microsoft.com/xmltools/2002/xmldiff\"><xd:node match=\"1\"><xd:add><b /></xd:add></xd:node></xd:xmldiff>";
             Assert.Equal(inner, expected);
+        }
+
+        [Fact]
+        public void SimpleFragment()
+        {
+            using var col = new TempFileCollection();
+            var file1 = col.CreateTempFile("<a></a><b></b>");
+            var file2 = col.CreateTempFile("<a></a><b></b><c></c>");
+
+            XmlDiff diff = new XmlDiff();
+
+            var sw = new StringWriter();
+            var settings = new XmlWriterSettings() { OmitXmlDeclaration = true, Indent = false };
+            using (var writer = XmlWriter.Create(sw, settings))
+            {
+                Assert.False(diff.Compare(file1, file2, true, writer));
+            }
+
+            XmlDocument result = new XmlDocument();
+            result.LoadXml(sw.ToString());
+
+            var inner = ToComparibleString(result); 
+            var expected = "<xd:xmldiff version=\"1.0\" options=\"None\" fragments=\"yes\" xmlns:xd=\"http://schemas.microsoft.com/xmltools/2002/xmldiff\"><xd:node match=\"2\" /><xd:add><c /></xd:add></xd:xmldiff>";
+            Assert.Equal(inner, expected);
+        }
+
+        private string ToComparibleString(XmlDocument doc)
+        {
+            // avoid comparing the hash.
+            var s = doc.OuterXml;
+            int pos = s.IndexOf("srcDocHash");
+            int end = s.IndexOf("\"", pos + 12) + 2;
+            s = s.Substring(0, pos) + s.Substring(end);
+            return s;
         }
     }
 }
